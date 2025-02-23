c13a44bcf19915a8519f06710a5de4683605b565
Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>
2025-02-23 13:29:40 +0800

    v0.4 优化编译体验。
    Signed-off-by: Gentlesprite <77564247+Gentlesprite@users.noreply.github.com>

--- build.py
+++ build.py
@@ -4,7 +4,9 @@
 # Time:2025/2/23 12:55
 # File:build.py
 import os
+import shutil
 
+software_name = 'scheduleder'
 main = 'main.py'
 icon_path = r'module\resources\logo.ico'
 upx_dir = r'module\bin\upx.exe'
@@ -12,13 +14,19 @@ version_file = os.path.join(os.getcwd(), 'file_version_info.txt')
 if __name__ == '__main__':
     try:
         import PyInstaller.__main__
+
+        PyInstaller.__main__.run([
+            '--upx-dir', upx_dir,
+            '-F',
+            '-w',
+            '-i', icon_path,
+            '--version-file', version_file,
+            '--name', software_name,
+            main
+        ])
     except ImportError:
         print('使用pip install pyinstaller安装pyinstaller后重试。')
-    PyInstaller.__main__.run([
-        '--upx-dir', upx_dir,
-        '-F',
-        '-w',
-        '-i', icon_path,
-        '--version-file', version_file,
-        main
-    ])
+    finally:
+        build_dir = os.path.join(os.getcwd(), 'build')
+        if os.path.exists(build_dir):
+            shutil.rmtree(build_dir)
